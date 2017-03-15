3a39164f9212b898be20dca5f5ca82135e0749bb
Arthur Livingston <Artielivingston1@gmail.com>
2017-03-15 13:45:25 -0500

    m

--- server/db.js
+++ server/db.js
@@ -2,9 +2,10 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'f1historiandata.c7dt2jerhu7u.us-west-2.rds.amazonaws.com',
   database: 'f1historiandata',
-  user:'"' + process.env.DB_USERNAME + '"',
-  password:'"' +  process.env.DB_PASSWORD + '"'
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 });
+console.log(connection);
 connection.connect((err) => {
   if(err) {
     console.log(`There was an error connecting to the DB, ${err}`);
