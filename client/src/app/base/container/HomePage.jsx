d9c65f7a2fb071eb7ad632bd6ccdf75bae97f61c
Arthur Livingston <Artielivingston1@gmail.com>
2017-03-23 16:42:16 -0500

    Update to scrolling

--- client/src/app/base/container/HomePage.jsx
+++ client/src/app/base/container/HomePage.jsx
@@ -4,26 +4,17 @@ import axios from 'axios';
 import Home from '../presentation/Home.jsx';
 import Footer from '../presentation/Footer.jsx';
 import News from './NewsPage.jsx';
-import smoothScroll from 'smoothScroll';
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-    this.scrollToNews = this.scrollToNews.bind(this);
-  }
-
-  scrollToNews(e) {
-    console.log(e);
-    e.preventDefault();
-    let topOfNews = document.querySelector('.news');
-    smoothScroll(topOfNews);
   }
 
   render() {
     return(
       <div>
-        <Home onClick={this.scrollToNews}/>
+        <Home />
         <News />
         <Footer />
       </div>
