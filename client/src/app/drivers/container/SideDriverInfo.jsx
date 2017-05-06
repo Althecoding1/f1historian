e9c067ba928c2dae46ff46aae400fc973c5f40af
Arthur Livingston <Artielivingston1@gmail.com>
2017-05-06 02:11:10 -0500

    add component for driver infocard

---
+++ client/src/app/drivers/container/SideDriverInfo.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SideDriverInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+module export SideDriverInfo;
