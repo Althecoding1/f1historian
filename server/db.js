17819313d76a2bdf6445362aa486c529d409dd14
Arthur Livingston <Artielivingston1@gmail.com>
2017-07-30 17:50:12 -0500

    db config file update

--- server/db.js
+++ server/db.js
@@ -10,23 +10,3 @@ client.connect( (err) => {
 });
 
 module.exports = client;
-
-
-
-//
-// const mysql = require('mysql');
-// const env = require('../.env');
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   database: 'f1sqldata',
-//   user: 'root',
-//   password: ''
-// });
-// connection.connect((err) => {
-//   if(err) {
-//     console.log(`There was an error connecting to the DB, ${err}`);
-//   } else {
-//     console.log(`Successfully connected to database!`);
-//   }
-// });
-// host: 'f1historiandata.c7dt2jerhu7u.us-west-2.rds.amazonaws.com',
